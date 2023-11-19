1430ee8e1a9c3e665da1c5890b3a401e60792d76
nikolaichem <nikolai@niuitmo.ru>
2023-11-19 22:53:56 +0300

    add: template for new features

---
+++ database/utils.py
@@ -0,0 +1,29 @@
+import base64
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+# import forgi.visual.mplotlib as fvm
+# import forgi
+
+
+def gc_count(sequence: str):
+    pass
+
+
+def second_structure_plot(sequence: str) -> None:
+    pass
+
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+
+
+
+
