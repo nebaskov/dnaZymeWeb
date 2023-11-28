7c7f8f6bb2b6edd75cd6d6c1efa179713a57aba5
Yevgeniya Din <djzhendogg.is.busy2@gmail.com>
2023-11-28 12:34:39 +0300

    start with bd

--- database/views.py
+++ database/views.py
@@ -4,7 +4,7 @@ from django.core import serializers
 
 
 def database_home(request):
-    # db = serializers.serialize("python", MainDnaDataBase.objects.all())
-    # return render(request, 'database/database_home.html', {'db': db})
-    return render(request, 'database/database_home.html')
+    db = serializers.serialize("python", MainDnaDataBase.objects.all())
+    return render(request, 'database/database_home.html', {'db': db})
+    # return render(request, 'database/database_home.html')
 
