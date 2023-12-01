3d6a1f9083bb46802fc72c98b803b9f396662a86
nikolaichem <nikolai@niuitmo.ru>
2023-12-01 17:28:53 +0300

    add database model candidates

--- database/models.py
+++ database/models.py
@@ -16,3 +16,18 @@ class MainDnaDataBase(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Candidates(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    sequence = models.CharField(max_length=96, null=False, default='')
+    activity = models.FloatField(null=False, default=0)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'candidate'
+        verbose_name_plural = 'candidates'
+        db_table = 'candidates'
+
+    def __str__(self):
+        return self.sequence
